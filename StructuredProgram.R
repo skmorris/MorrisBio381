5f5fb018204307063cf51e770f96ae1db78ac431
skmorris <skmorris91@gmail.com>
2021-04-25 09:23:26 -0400

    updating HW 9

---
+++ StructuredProgram.R
@@ -0,0 +1,17 @@
+# program for HW9
+# 7 April 2021
+# SKM
+#--------------------------------------------
+# Preliminaries
+library(ggplot2)
+set.seed(99) # makes the code reproducible with the same results
+source("")
+
+#--------------------------------------------
+# Global variables
+blahFile <- "blah.csv"
+xCol <- 7
+yCol <- 5
+#---------------------------------------------
+# Program body
+
